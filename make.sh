ff3dc6e370c7fb0630b4761c645760439d2a2bb4
Eogrim <christophe.hardy@isen-bretagne.fr>
2018-07-26 10:51:36 -0400

    Automatic daily save at 20180726

--- make.sh
+++ make.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 #Bash script to make the mediawiki
 
-echo "-------Git Large Files Storage install-------"
+#echo "-------Git Large Files Storage install-------"
 #need? curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
-sudo apt-get update
-sudo apt-get install git-lfs
-git lfs install
+#sudo apt-get update
+#sudo apt-get install git-lfs
+#git lfs install
 
 echo "-------Pulling files-------"
 git pull
 
-echo "-------Tracking larges files-------"
-sudo git-lfs track "wikidb/**"
-sudo git-lfs track "images/**"
+#echo "-------Tracking larges files-------"
+#sudo git-lfs track "wikidb/**"
+#sudo git-lfs track "images/**"
 
 echo "-------Creating Backup Crontab-------"
 crontab -l | { cat; echo "*/5 * * * * bash git-backup.sh"; } | crontab -
