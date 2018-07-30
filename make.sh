ecd113fffae202f4bee52b2af5a1712d4a26e678
Eogrim <christophe.hardy@isen-bretagne.fr>
2018-07-30 14:37:03 -0400

    Automatic daily save on 20180730

--- make.sh
+++ make.sh
@@ -18,6 +18,7 @@ echo "-------Pulling files-------"
 
 echo "-------Creating Backup Crontab-------"
 path=$(cd $( dirname ${BASH_SOURCE[0]}) && pwd )
+chmod +x ./git-backup.sh
 crontab -l | { cat; echo "* * * * * /bin/bash $path/git-backup.sh"; } | crontab -
 
 #echo "-------Powering the wiki with the dockers-------"
