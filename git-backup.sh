d75a44061bd5f1bada93bb641ec13c57778e746a
Eogrim <christophe.hardy@isen-bretagne.fr>
2018-07-30 14:45:02 -0400

    Automatic daily save on 20180730

--- git-backup.sh
+++ git-backup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #Bash script to save and push to git the FNNDSC mediawiki
-PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin
 pathscript=/neuro/labs/grantlab/research/mediawiki
 jour=$(date +%Y%m%d)
-#cd $path && docker-compose down
+#cd $pathscript && docker-compose down
 cd $pathscript && git add -A
 cd $pathscript && git commit -m "Automatic daily save on $jour"
 cd $pathscript && git push
-#cd $path && docker-compose up
+#cd $pathscript && docker-compose up
