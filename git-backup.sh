96d8d7e338b933afc74fb60ad6af486c15370494
Eogrim <christophe.hardy@isen-bretagne.fr>
2018-07-30 15:29:02 -0400

    Automatic daily save on 20180730

--- git-backup.sh
+++ git-backup.sh
@@ -3,7 +3,8 @@
 pathscript=/neuro/labs/grantlab/research/mediawiki
 jour=$(date +%Y%m%d)
 #cd $pathscript && docker-compose down
-cd $pathscript && git add -A > /var/log/backuplog.log
-cd $pathscript && git commit -m "Automatic daily save on $jour" > /var/log/backuplog.log
-cd $pathscript && git push > /var/log/backuplog.log
+echo "test" >> /var/log/backuplog.log
+cd $pathscript && git add -A >> /var/log/backuplog.log
+cd $pathscript && git commit -m "Automatic daily save on $jour" >> /var/log/backuplog.log
+cd $pathscript && git push >> /var/log/backuplog.log
 #cd $pathscript && docker-compose up
