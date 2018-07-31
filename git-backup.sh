cba21ac92ff9e0122df19b5c247885b0458f753e
Eogrim <christophe.hardy@isen-bretagne.fr>
2018-07-31 09:34:07 -0400

    Automatic daily save on 20180731

--- git-backup.sh
+++ git-backup.sh
@@ -2,14 +2,14 @@
 #Bash script to save and push to git the FNNDSC mediawiki
 pathscript=/neuro/labs/grantlab/research/mediawiki
 logfile=/tmp/backup.log
-jour=$(date +%Y%m%d)
-cd $pathscript && docker-compose down
+date=$(date +%Y%m%d)
+#cd $pathscript && docker-compose down
 echo -e "\n\n$jour" >> $logfile
 echo "Adding files..." >>  $logfile
 git -C $pathscript add -A >> $logfile
 echo "Committing files..." >>  $logfile
-git -C $pathscript commit -m "Automatic daily save on $jour" >> $logfile
+git -C $pathscript commit -m "Automatic daily save on $date" >> $logfile
 echo "Pushing files..." >>  $logfile
 git -C $pathscript push origin master >> $logfile
 echo "Done!" >>  $logfile
-cd $pathscript && docker-compose up
+#cd $pathscript && docker-compose up
--- make.sh
+++ make.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 #Bash script to install and powerup the FNNDSC mediawiki
 
+echo "-------Initialization-------"
+
 #echo "-------Git Large Files Storage install-------"
 #sudo apt-get update
 #sudo apt-get install git-lfs
 #git lfs install
 
-echo "-------Pulling files-------"
-git pull
-git fetch --all
-git reset --hard origin/master
+#echo "-------Pulling files-------"
+#git pull
+#git fetch --all
+#git reset --hard origin/master
 
 #echo "-------Tracking larges files-------"
 #sudo git-lfs track "wikidb/**"
@@ -19,7 +21,7 @@ echo "-------Creating Backup Crontab-------"
 path=$(cd $( dirname ${BASH_SOURCE[0]}) && pwd )
 chmod +x ./git-backup.sh
 crontab -l | { cat; echo "PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin"; } | crontab -
-crontab -l | { cat; echo "00 04 * * * sudo -u $(whoami) $path/git-backup.sh"; } | crontab -
+crontab -l | { cat; echo "* * * * * sudo -u $(whoami) $path/git-backup.sh"; } | crontab -
 
 echo "-------Powering the wiki with the dockers-------"
-docker-compose up
+#docker-compose up
