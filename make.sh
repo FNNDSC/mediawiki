72704728a8dbdc2cae36dbb5c1d717798782de82
Eogrim <christophe.hardy@isen-bretagne.fr>
2018-07-31 09:36:03 -0400

    Automatic daily save on 20180731

--- make.sh
+++ make.sh
@@ -8,7 +8,7 @@ echo "-------Initialization-------"
 #sudo apt-get install git-lfs
 #git lfs install
 
-#echo "-------Pulling files-------"
+#echo "-------Pulling files latest files-------"
 #git pull
 #git fetch --all
 #git reset --hard origin/master
@@ -21,7 +21,7 @@ echo "-------Creating Backup Crontab-------"
 path=$(cd $( dirname ${BASH_SOURCE[0]}) && pwd )
 chmod +x ./git-backup.sh
 crontab -l | { cat; echo "PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin"; } | crontab -
-crontab -l | { cat; echo "* * * * * sudo -u $(whoami) $path/git-backup.sh"; } | crontab -
-
+crontab -l | { cat; echo "* * * * * $path/git-backup.sh"; } | crontab -
+#sudo -u $(whoami)
 echo "-------Powering the wiki with the dockers-------"
 #docker-compose up
